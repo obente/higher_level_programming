44cc2f5ffe56b90f5c6716b7a58a9c85608b3662
obinna E <obinnaekenonu@gmail.com>
2023-01-03 23:20:42 +0000

    9-print-last_digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,8 +3,3 @@
 def print_last_digit(number):
     print("{}".format(str(number)[-1]), end="")
     return (str(number)[-1])
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)

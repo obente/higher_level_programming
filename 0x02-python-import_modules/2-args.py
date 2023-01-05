76a1b04d7dc5d4d475439ee8284942c834c0354c
obinna E <obinnaekenonu@gmail.com>
2023-01-05 17:18:30 +0000

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def main():
+    from sys import argv
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print(len(argv) - 1, "argument:")
+    else:
+        print(len(argv) - 1,"arguments:")
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()

29a5014b258ceec6abe4dae470fc93d3b8f543be
obinna E <obinnaekenonu@gmail.com>
2023-01-05 17:51:06 +0000

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def main():
+    import hidden_4
+    
+
+    names = dir(hidden_4)
+    for name in names:
+        if name.startswith("__"):
+            continue
+        else:
+            print(name)
+
+if __name__ == "__main__":
+    main()
+

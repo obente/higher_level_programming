686ac2c84e1f690e3c6595a9ae0142b28bd718f6
obinna E <obinnaekenonu@gmail.com>
2023-01-25 15:21:19 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Define class called Square """
+
+
+class Square:
+    """ Square class which does nothing """
+    def __init__(self, size=0):
+        """ Initialize square """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2

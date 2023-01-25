107851199dd4216aec8fe5632d9137f6196919fe
obinna E <obinnaekenonu@gmail.com>
2023-01-25 15:09:47 +0000

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Define class called Square """
+
+
+class Square:
+    """ Square class which does nothing """
+    def __init__(self, size=0):
+        """ Initialize square """
+        if size not instanceof(int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size

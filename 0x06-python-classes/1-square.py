ae4761f7aabd7a36269dc0640aa9369303e0f83e
obinna E <obinnaekenonu@gmail.com>
2023-01-25 15:04:20 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Define class called Square """
+
+
+class Square:
+    """ Square class which does nothing """
+    def __init__(self, size):
+        """ Initialize square """
+        self.__size = size

8b93977b9465f2cc06f2eb059365ca019d501da1
obinna E <obinnaekenonu@gmail.com>
2023-02-07 04:42:50 +0000

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,8 +7,8 @@ class MyInt(int):
 
     def __eq__(self, value):
         """Override == opeartor with != operator behavior."""
-        return self.val != value
+        return self.real != value
 
     def __ne__(self, value):
         """Override != operator with == operator behavior."""
-        return self.val == value
+        return self.real == value

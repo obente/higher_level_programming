8d46c453dd6a8034a3cc9d51e56b06e6835bcc9a
obinna E <obinnaekenonu@gmail.com>
2023-02-08 15:27:08 +0000

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function to write UTF8 textfile"""
+
+
+def read_file(filename="", text=""):
+    """ Write a file and return number of string written
+
+    Keyword argumwnts
+    filename - file to be write to (defaults to empty string)
+    text - string content to write (defaults to empty string)
+    """
+    with open(filename, text, encoding="utf-8") as f:
+        print(f.write(), end="")

62b1c8dc95f33ba2333042569c9f24a1beada31a
obinna E <obinnaekenonu@gmail.com>
2023-02-08 15:02:38 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+    """ Read a file and print to stdout
+    
+    Keyword argumwnts
+    filename - file to be read (defaults to empty string
+    """
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     

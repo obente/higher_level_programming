53eb00f026e200b92156725296e96baaa2ae3d1e
obinna E <obinnaekenonu@gmail.com>
2023-04-02 22:49:11 +0000

    fix condition for empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-
-    big_key = ''
-    val = float('-inf')
-    for k, v in a_dictionary.items():
-        if v > val:
-            val = v
-            big_key = k
-    return big_key
+    if isinstance(a_dictionary, dict) and a_dictionary != {}:
+        big_key = ''
+        val = float('-inf')
+        for k, v in a_dictionary.items():
+            if v > val:
+                val = v
+                big_key = k
+        return big_key
+    return None

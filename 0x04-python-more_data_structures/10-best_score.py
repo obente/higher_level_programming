4a86619d001fe17e4f7d7522bdf685e4b27bd148
obinna E <obinnaekenonu@gmail.com>
2023-04-02 21:17:42 +0000

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict)  or len(a_dictionary) == 0:
+    if a_dictionary is None:
         return None
-    
-    key = list(a_dictionary.keys())[0]
-    max_v = a_dictionary[key]
+
+    big_key = ''
+    val = float('-inf')
     for k, v in a_dictionary.items():
-        if v > max_v:
-            max_v = v
-            max_k = k
-    return max_k
+        if v > val:
+            val = v
+            big_key = k
+    return big_key

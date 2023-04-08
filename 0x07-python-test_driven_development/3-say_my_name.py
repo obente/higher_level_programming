b3e5cbaa42f164c164a85267100b73206ef9c78f
obinna E <obinnaekenonu@gmail.com>
2023-04-08 16:21:31 +0000

    split isinstance test and compyied with pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,10 +3,11 @@
 This module prints full names of persons
 '''
 
+
 def say_my_name(first_name, last_name=""):
     '''
     returns full name when given first_name and last_name
-    
+
     Args:
     first_name (str): firstname of person
     last_name (str): lastname of person
@@ -18,7 +19,8 @@ def say_my_name(first_name, last_name=""):
     (str): concatenated first_name and last_name with additional text
     '''
 
-    if not (isinstance(first_name, str) and isinstance(last_name, str)):
-        raise TypeError("first_name must be a string or last_name must be a string")
+    if not (isinstance(first_name, str)):
+        raise TypeError("first_name must be a string")
+    if not (isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
     return f"My name is {first_name} {last_name}"
-

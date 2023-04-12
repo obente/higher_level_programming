4cf00792e19ac533aaaff217c9b225b2439abcae
obinna E <obinnaekenonu@gmail.com>
2023-04-12 23:14:34 +0000

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let k = '';
+      for (let j = 0; j < this.width; j++) {
+        k = k + 'X';
+      }
+      console.log(k);
+    }
+  }
+
+  rotate () {
+    const width = this.height;
+    this.height = this.width;
+    this.width = width;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;

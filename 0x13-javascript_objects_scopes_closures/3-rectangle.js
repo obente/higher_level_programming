61b7ad6fe23d7b9ed1e671a7891e46fb852bb66d
obinna E <obinnaekenonu@gmail.com>
2023-04-12 22:46:20 +0000

    print squares

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let k = '';
+      for (let j = 0; j < this.width; j++) {
+	    k = k + 'X';
+      }
+	    console.log(k);
+    }
+  }
+}
+module.exports = Rectangle;

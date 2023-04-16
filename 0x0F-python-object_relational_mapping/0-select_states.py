fff3ee468be4ccb173e08e7f1af93ebe5f94822b
obinna E <obinnaekenonu@gmail.com>
2023-04-16 20:35:04 +0000

    added port and host param

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    from sys import argv
+def main():
     import MySQLdb
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    from sys import argv
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], host='localhost', port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY id")
     states = cur.fetchall()
 
     for data in states:
         print(data)
-
+    
+    cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()

e9d27f15c7739e19083f8c466bd06c18ed939090
obinna E <obinnaekenonu@gmail.com>
2023-04-16 22:07:37 +0000

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Get list of cities with their states
+"""
+
+
+def main():
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                         db=argv[3], host='localhost', port=3306)
+
+    cur = db.cursor()
+    cur.execute("""SELECT c.id, c.name, s.name FROM cities c
+                JOIN states s
+                ON c.id = s.id;""")
+    states = cur.fetchall()
+
+    for data in states:
+        print(data)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()

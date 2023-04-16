17f1365f1aa235a9735ca684e09317e23dc49b2d
obinna E <obinnaekenonu@gmail.com>
2023-04-16 21:51:43 +0000

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Get list of states that is searched for safely
+"""
+
+
+def main():
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                         db=argv[3], host='localhost', port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (argv[4],))
+    states = cur.fetchall()
+
+    for data in states:
+        print(data)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
